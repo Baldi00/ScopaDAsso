0ffb7bc474b586dfc3a6ebab6826a7613b8d00ec
Baldi00 <andrea.baldelli2000@gmail.com>
2022-06-27 17:37:30 +0200

    model: added initial card picking from field logic

--- src/scopadasso/model/GameManager.java
+++ src/scopadasso/model/GameManager.java
@@ -1,6 +1,7 @@
 package scopadasso.model;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class GameManager {
@@ -52,5 +53,90 @@ public class GameManager {
 
     public void humanPlayerPlayCard(Card card) {
         humanPlayer.playCard(card);
+        executeMove(humanPlayer, card);
+    }
+
+    private void executeMove(Player player, Card card) {
+        boolean done = searchAndGrabSingleCard(player, card);
+
+        if(!done){
+            done = searchAndGrabMultipleCards(player, card);
+        }
+    }
+
+    private boolean searchAndGrabSingleCard(Player player, Card card){
+        for(Card card1 : field){
+            if(card.getValue().equals(card1.getValue())){
+                if(field.size() == 1){
+                    player.addToMop(card);
+                }else{
+                    player.addToBank(card);
+                }
+                player.addToBank(card1);
+                field.remove(card1);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean searchAndGrabMultipleCards(Player player, Card card){
+        List<List<Card>> possibleCardsToGrab = searchForPossibleCardsToGrab(card);
+        return false;
+    }
+
+    private List<List<Card>> searchForPossibleCardsToGrab(Card card) {
+        List<List<Card>> permutations = new ArrayList<>();
+        for (int i=1; i<=field.size(); i++){
+            List<Card> combination = new ArrayList<>();
+            createPermutation(permutations, combination, field, i, card.getValue().getValue());
+        }
+
+        deleteDuplications(permutations);
+        return permutations;
+    }
+
+    private void deleteDuplications(List<List<Card>> permutations) {
+        for(List<Card> list : permutations){
+            Collections.sort(list);
+        }
+
+        List<List<Card>> permutationsWithoutDuplications = new ArrayList<>();
+        int permutationsWithoutDuplicationsSize = 0;
+        if(!permutations.isEmpty()) {
+            permutationsWithoutDuplications.add(permutations.get(0));
+            permutationsWithoutDuplicationsSize++;
+            for (int i = 1; i < permutations.size(); i++) {
+                if(!permutationsWithoutDuplications.get(permutationsWithoutDuplicationsSize-1).containsAll(permutations.get(i))){
+                    permutationsWithoutDuplications.add(permutations.get(i));
+                    permutationsWithoutDuplicationsSize++;
+                }
+            }
+        }
+    }
+
+    private void createPermutation(List<List<Card>> result, List<Card> partial, List<Card> available, int length, int cardValue) {
+        if (partial.size() == length) {
+            if(calculateValueOfCards(partial) == cardValue)
+                result.add(partial);
+            return;
+        }
+        for (Card card : available) {
+            List<Card> partial2 = new ArrayList<>(partial);
+            partial2.add(card);
+            if(calculateValueOfCards(partial2) <= cardValue) {
+                List<Card> available2 = new ArrayList<>(available);
+                available2.remove(card);
+                createPermutation(result, partial2, available2, length, cardValue);
+            }
+        }
+    }
+
+    private int calculateValueOfCards(List<Card> cards) {
+        int counter = 0;
+        for(Card card : cards){
+            counter += card.getValue().getValue();
+        }
+        return counter;
     }
 }
