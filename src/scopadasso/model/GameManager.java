bb60f6c806c6265372c47bac9e3e51ea8c8e2716
Baldi00 <andrea.baldelli2000@gmail.com>
2022-06-27 13:02:30 +0200

    model: added GameManager with game initialization

---
+++ src/scopadasso/model/GameManager.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scopadasso.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Andrea
+ */
+public class GameManager {
+    private final Player humanPlayer;
+    private final Player cpuPlayer;
+    private final List<Card> field;
+    private final Deck deck;
+    
+    public GameManager() {
+        humanPlayer = new Player(0);
+        cpuPlayer = new Player(1);
+        field = new ArrayList<>();
+        deck = new Deck();
+    }
+    
+    public void preparation() {
+        deck.shuffleDeck();
+        prepareField();
+        giveThreeCardsToPlayers();
+    }
+    
+    private void prepareField(){
+        for(int i=0; i<4; i++)
+            field.add(deck.extract());
+    }
+    
+    public void giveThreeCardsToPlayers(){
+        for(int i=0; i<3; i++) {
+            humanPlayer.receiveCard(deck.extract());
+            cpuPlayer.receiveCard(deck.extract());
+        }
+    }
+}
