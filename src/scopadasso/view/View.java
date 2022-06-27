4f31c4e4f1e61847bbf8a129e6d5bf486fab9193
Baldi00 <andrea.baldelli2000@gmail.com>
2022-06-27 13:49:44 +0200

    view: added setters and components

--- src/scopadasso/view/View.java
+++ src/scopadasso/view/View.java
@@ -9,6 +9,7 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.util.List;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -17,6 +18,8 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import scopadasso.model.Card;
+import scopadasso.model.Deck;
 
 /**
  *
@@ -24,23 +27,39 @@ import javax.swing.SwingConstants;
  */
 public class View {
     private JFrame frame;
-    private JPanel mainPanel, centralPanel, humanPlayerPanel, cpuPlayerPanel;
+    private JPanel mainPanel, fieldPanel, humanPlayerPanel, cpuPlayerPanel;
+    private JLabel [] humanPlayerCardsLabels, cpuPlayerCardsLabels, fieldLabels;
+    private JLabel deckLabel;
     
     public View(){
         //INITIALIZATIONS
         frame = new JFrame("Scopa d'Asso");
         mainPanel = new JPanel(new BorderLayout());
-        centralPanel = new JPanel(new GridLayout(2, 4, 0, 0));
+        fieldPanel = new JPanel(new GridLayout(2, 4, 0, 0));
         humanPlayerPanel = new JPanel(new GridLayout(1, 3, 0, 0));
         cpuPlayerPanel = new JPanel(new GridLayout(1, 3, 0, 0));
         
-        cpuPlayerPanel.add(new JLabel("Card1"));
-        cpuPlayerPanel.add(new JLabel("Card2"));
-        cpuPlayerPanel.add(new JLabel("Card3"));
+        humanPlayerCardsLabels = new JLabel[3];
+        cpuPlayerCardsLabels = new JLabel[3];
+        
+        for(int i=0; i<3; i++) {
+            humanPlayerCardsLabels[i] = new JLabel();
+            cpuPlayerCardsLabels[i] = new JLabel();
+            humanPlayerPanel.add(humanPlayerCardsLabels[i]);
+            cpuPlayerPanel.add(cpuPlayerCardsLabels[i]);
+        }
+        
+        fieldLabels = new JLabel[8];
+        for(int i=0; i<8; i++) {
+            fieldLabels[i] = new JLabel();
+            fieldPanel.add(fieldLabels[i]);
+        }
+        
+        deckLabel = new JLabel();
         
         //ASSEMBLY
         mainPanel.add(humanPlayerPanel, BorderLayout.WEST);
-        mainPanel.add(centralPanel, BorderLayout.CENTER);
+        mainPanel.add(fieldPanel, BorderLayout.CENTER);
         mainPanel.add(cpuPlayerPanel, BorderLayout.EAST);
         
         frame.add(mainPanel);
@@ -50,5 +69,53 @@ public class View {
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+    }
+
+    public void setHumanPlayerCards(List<Card> hand) {
+        for (int i = 0; i < 3; i++) {
+            humanPlayerCardsLabels[i].setText("");
+        }
+        for (int i = 0; i < hand.size(); i++) {
+            humanPlayerCardsLabels[i].setText(hand.get(i).getValue() + " " + hand.get(i).getSeed());
+        }
+    }
+
+    public void setHumanPlayerBank(List<Card> bankWithoutMop) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+    }
+
+    public void setHumanPlayerMop(List<Card> mop) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void setCpuPlayerCards(List<Card> hand) {
+        for (int i = 0; i < 3; i++) {
+            cpuPlayerCardsLabels[i].setText("");
+        }
+        for (int i = 0; i < hand.size(); i++) {
+            cpuPlayerCardsLabels[i].setText("Card"+i);
+        }
+    }
+
+    public void setCpuPlayerBank(List<Card> bankWithoutMop) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void setCpuPlayerMop(List<Card> mop) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void setDeck(Deck deck) {
+        deckLabel.setText(""+deck.size());
+    }
+
+    public void setField(List<Card> field) {
+        for (int i = 0; i < 8; i++) {
+            fieldLabels[i].setText("");
+        }
+        for (int i = 0; i < field.size(); i++) {
+            fieldLabels[i].setText(field.get(i).getValue() + " " + field.get(i).getSeed());
+        }
     }
 }
